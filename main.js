3f80bf5f2a18ce9c4aa15a6241e16fa41ef61b4c
agub <aguhwtf1@gmail.com>
2023-07-10 18:04:22 -0300

    function

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-alert("Bienvenido a un simulador de prestamos");
+
+function calcularPrestamo(){
+    alert("Bienvenido a un simulador de prestamos");
 
 let monto = parseFloat(prompt("Ingresa el dinero"));
 let cuotas = parseInt(prompt("Ingrese las cuotas, la taza de impuesto es del 10%"));
@@ -13,4 +15,11 @@ for (let i = 1; i <= cuotas; i++) {
 let costoFinal = montoConImpuesto / cuotas;
 alert("Cuota mensual " + i + ": $" + costoFinal.toFixed(2));
 }
+if (cuotas >= 12) {
+    alert("Decidiste usar una finaciacion en 12 cuotas")
+} else {
+    alert("Gracias por utilizar aguprestamos")
+}
+}
 
+calcularPrestamo();
